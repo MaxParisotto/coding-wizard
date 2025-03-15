7c76bbea8fa81042315765c40cd40454302d7c82
MaxParisotto <max.parisotto@icloud.com>
2025-03-15 19:39:16 +0100

    Add ESLint configuration for TypeScript with custom rules

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    plugins: {
+      '@typescript-eslint': ts
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-unused-vars': 'error'
+    }
+  }
+];
